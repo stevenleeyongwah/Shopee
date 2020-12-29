45409e0a244f4df7f81f80b575db5c574fedc463
Steven Lee <stevenleeyongwah@gmail.com>
2020-12-30 02:57:02 +0800

    Navbar fix

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -9,39 +9,75 @@ import { logout } from 'actions/authActions';
 class Navbar extends Component {
   render() {
     return (
-      <div class="">
+      <div class="navbar">
         {/* Top navbar */}
-        <div className="">
-          <div className="">
+        <div className="top-navbar">
             {/* Top left navbar */}
             <div className="top-left-navbar">
               {/* Seller center */}
-              <NavLink to="#">Seller Center</NavLink>
+              <NavLink className="seller-center" to="#">Seller Center</NavLink>
               {/* Sell on Shopee */}
-              <NavLink to="#">Sell on Shopee</NavLink>
+              <NavLink className="sell-on-shopee" to="#">Sell on Shopee</NavLink>
               {/* Download */}
-              <div>Download</div>
+              <div className="download">Download</div>
               {/* Follow us on */}
-              <div>
-                Follow us on
-                <i class="ml-2 fab fa-facebook"></i>
-                <i class="ml-2 fab fa-instagram"></i>
+              <div className="follow-us-on">Follow us on</div>
+              {/* Logo */}
+              <div className="logo">
+                <i className="ml-2 fab fa-facebook"></i>
+                <i className="ml-2 fab fa-instagram"></i>
               </div>
+
             </div>
             {/* Spacer */}
-            <div className="">
-            
-            </div>
+            <div className="top-navbar-spacer"></div>
             {/* Top right navbar */}
-            <div className="">
-            
+            <div className="top-right-navbar">
+              <ul>
+                <li className="">
+                  <NavLink className="notifications" to="#">
+                    <i className="far fa-bell"></i>
+                    <span>Notifications</span>
+                  </NavLink>
+                </li>
+                <li className="">
+                  <NavLink className="help" to="#">
+                    <i className="far fa-question-circle"></i>
+                    <span>Help</span>
+                  </NavLink>
+                </li>
+                <li className="">
+                  <NavLink className="user" to="#">
+                    <i class="fas fa-user-circle text-light"></i>
+                    <span>stevenleeyongwah96</span>
+                  </NavLink>
+                </li>
+              </ul>
             </div>
-          </div>
         </div>
 
         {/* Bottom navbar */}
-        <div className="">
+        <div className="bottom-navbar">
+          {/* Shopee logo */}
+          <NavLink className="" to="#">
 
+          </NavLink>
+          {/* Search bar section */}
+          <div className="bottom-middle-navbar">
+            <div className="search-bar">
+              <div>
+                <form>
+                  <input />
+                </form>
+              </div>
+              <button></button>
+            </div>
+            <div className="items"></div>
+          </div>
+          {/* Shopping cart */}
+          <div className="bottom-right-navbar">
+            <div className="shopping-cart"></div>
+          </div>
         </div>
       </div>
     )
