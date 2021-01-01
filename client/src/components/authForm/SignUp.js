866e810be0c6d64b5b9becaf4939496ae5278781
Steven Lee <stevenleeyongwah@gmail.com>
2021-01-01 18:39:11 +0800

    Sign up page improve

--- client/src/components/authForm/SignUp.js
+++ client/src/components/authForm/SignUp.js
@@ -11,7 +11,23 @@ import { register } from 'actions/authActions'
 
 
 function SignUp() {
-  const [ phoneNumber, setPhoneNumber ] = useState('')
+  function changeInputDisplay(
+    greenTick, 
+    errorMessage,
+    borderColour,
+    backgroundColour,
+    buttonDisabled,
+    buttonCursor,
+    buttonOpacity
+    ) {
+      document.getElementsByClassName("correct-green-tick")[0].style.display = greenTick;
+      document.getElementsByClassName("phone-number-input-text-error")[0].innerHTML = errorMessage;
+      document.getElementsByClassName("phone-number-input-box")[0].style.border = borderColour;
+      document.getElementsByClassName("phone-number-input")[0].style.background = backgroundColour;
+      document.getElementsByClassName("submit-form-button")[0].disabled = buttonDisabled;
+      document.getElementsByClassName("submit-form-button")[0].style.cursor = buttonCursor;
+      document.getElementsByClassName("submit-form-button")[0].style.opacity = buttonOpacity;
+  }
 
   const handleSubmit = (e) => {
     // Prevent page from refreshing when submit form
@@ -20,55 +36,83 @@ function SignUp() {
     
   }
 
-  useEffect(() => {
-    // 2 Steps in validating phone number
-    if (phoneNumber.match(/(8|9)\d{7}$/g) !== null) {
-      console.log("regex: ", phoneNumber.match(/(8|9)\d{7}$/g)[0])
-
-      var prefixPhoneNumber = phoneNumber.split(/(8|9)\d{7}$/)[0].trim()
-      console.log(prefixPhoneNumber)
-      var correctPrefix = ["65", "(65)", "(+65)", "+65", ""]
-      console.log(correctPrefix.includes(prefixPhoneNumber))
-    } else {
-      console.log("Phone num not valid")
-    }
-
-    // var string4 = 'one split two splat three splot'
-    // var splitString4 = string4.split(/\s+(?:split|splat|splot)\s+/);
-    // console.log(splitString4);
-    
-
-    // Display success or error message here
-  }, [phoneNumber]);
-
-  const validatePhoneNumber = (phoneNumber) => {
-
-  }
-
   const handleChange = (e) => {
+    // Remove all spaces from input
+    var tempPhoneNumber = e.target.value.replace(/\s/g,'')
+    
+    // If phone number is empty, perform the following
+    if (tempPhoneNumber === ""){
+      changeInputDisplay("none","","1px solid rgba(0,0,0,.25)","#fff",true,"not-allowed","0.7")
+    }
+    
+    // If phone number is not empty, perform the following 
+    else {
+      
+      // If phone number matches (8|9)*** ****
+      if (tempPhoneNumber.match(/(8|9)\d{7}$/g) !== null) {
+        
+        // Extract phone number prefix
+        var prefixPhoneNumber = tempPhoneNumber.split(/(8|9)\d{7}$/)[0].trim()
 
+        // Define correct phone number prefix in an array
+        var correctPrefix = ["65", "(65)", "(+65)", "+65", ""]
+
+        // If user enters correct phone number prefix, perform the following
+        if (correctPrefix.includes(prefixPhoneNumber) == true){
+          changeInputDisplay("flex","","1px solid rgba(0,0,0,.25)","#fff",false,"pointer","1")
+
+          // Change text of input box with standard singapore prefix (+65)
+          document.getElementsByClassName("phone-number-input")[0].value = "(+65) " + tempPhoneNumber.match(/(8|9)\d{7}$/g);
+        } else if (correctPrefix.includes(prefixPhoneNumber) == false){
+          changeInputDisplay("none","Invalid Phone","1px solid #ee4d2d","#fff6f7",true,"not-allowed","0.7")
+        }
+      } else {
+        changeInputDisplay("none","Invalid Phone","1px solid #ee4d2d","#fff6f7",true,"not-allowed","0.7")
+      }
+    }
   }
 
   return (
     <div className="sign-up-page">
       {/* Header */}
       <Header headerText="Sign Up" />
+      
       {/* Body */}
       <div className="sign-up-body">
         <div className="image-body">
           <form>
             <div className="form-wrapper">
+              
+              {/* form title */}
               <div className="form-title">
                 <span>Sign Up</span>
               </div>
+
+              {/* form body */}
               <div className="form-body">
-                <input className="phone-number-input input-error" name="phoneNumber" placeholder="Phone Number" onChange={(e) => setPhoneNumber(e.target.value.replace(/\s/g,''))} />
+
+                {/* Phone number input */}
+                <div className="phone-number-section">
+                  <div className="phone-number-input-box" >
+                    <input className="phone-number-input" type="text" name="phoneNumber" placeholder="Phone Number" onChange={handleChange} />
+                    <div className="correct-green-tick">
+                      <svg fill="none" viewBox="0 0 16 16" class="stardust-icon stardust-icon-success-with-circle _1a0pcZ"><path fill="none" stroke="#6C0" d="M8 15A7 7 0 108 1a7 7 0 000 14z" clip-rule="evenodd"></path><path stroke="none" fill="#6C0" fill-rule="evenodd" d="M11.621 6.406l-3.98 4.059c-.266.266-.719.244-1.012-.049-.293-.293-.314-.746-.048-1.012l3.98-4.059c.266-.266.719-.245 1.012.048.293.293.314.747.048 1.013z" clip-rule="evenodd"></path><path stroke="none" fill="#6C0" fill-rule="evenodd" d="M3.803 7.997l2.81 2.532c.267.267.72.245 1.013-.048.293-.293.315-.746.048-1.012l-2.81-2.532c-.267-.267-.72-.245-1.013.048-.293.293-.314.746-.048 1.012z" clip-rule="evenodd"></path></svg>
+                    </div>
+                  </div>
+                  <div className="phone-number-input-text-error"></div>
+                </div>
+
+                {/* Submit form button */}
                 <button className="submit-form-button">Next</button>
+                
+                {/* Seperator */}
                 <div className="seperator">
                   <div className="left"></div>
                   <span className="middle">OR</span>
                   <div className="right"></div>
                 </div>
+                
+                {/* Others sign up method */}
                 <div className="others-sign-up-method">
                   <button className="facebook-button">
                     <div className="logo-wrapper">
@@ -89,6 +133,8 @@ function SignUp() {
                     <div>Apple</div>
                   </button>                    
                 </div>
+                
+                {/* Sign up policy text */}
                 <div className="sign-up-policy">
                   <span>By signing up, you agree to Shopee's </span>
                   <NavLink to="">Terms of Service </NavLink>
@@ -96,6 +142,8 @@ function SignUp() {
                   <NavLink to="">Privacy Policy</NavLink>
                 </div>
               </div>
+              
+              {/* Log in if already have an account */}
               <div className="form-footer">
                 <span>Have an account?</span>
                 <NavLink to="">  Log In</NavLink>
@@ -119,52 +167,3 @@ export default connect(
   mapStateToProps,
   { register }
 )(SignUp)
-
-// {/* <div className="d-flex flex-column align-items-center body">
-//           <div className="card" style={{width: "40rem"}}>
-//             <div className="card-body">
-//               {/* Title */}
-//               <h5 className="card-title title">Sign Up</h5>
-
-//               {/* sign up form */}
-//               <form onSubmit={handleSubmit}>
-//                 {/* Username */}
-//                 <div class="mb-3">
-//                   <input type="text" name="name" className="form-control" style={{borderColor: "grey"}} autoFocus="autofocus" placeholder="Username" onChange={onChange} />
-//                   <div class="name-error invalid">{ this.state.msg ? this.state.msg.name : null }</div>
-//                 </div>
-
-//                 {/* Email */}
-//                 <div class="mb-3">
-//                   <input type="text" name="email" className="form-control" style={{borderColor: "grey"}} placeholder="Email" onChange={onChange} />
-//                   <div class="email-error invalid">{ this.state.msg ? this.state.msg.email : null }</div>
-//                 </div>
-
-//                 {/* Password */}
-//                 <div class="mb-3">
-//                   <input type="password" name="password" className="form-control" style={{borderColor: "grey"}} placeholder="Password" onChange={onChange} />
-//                   <div class="password-error invalid">{ this.state.msg ? this.state.msg.password : null }</div>
-//                 </div>
-
-//                 {/* Sign Up */}
-//                 <button type="submit" className="btn btn-block signUpButton mb-1 mt-3" >Sign Up</button>
-//               </form>
-
-//               {/* Divider */}
-//               <div className="separator">OR</div>
-
-//               {/* Sign Up with facebook, google, apple */}
-//               <div className="signUpWith">
-//                 <button className="facebook"><i className="fab fa-facebook-f mr-4"></i>Facebook</button>
-//                 <button className="google"><i className="fab fa-google mr-4"></i>Google</button>
-//                 <button className="apple"><i className="fab fa-apple mr-4"></i>Apple</button>
-//               </div>
-
-//               {/* Sign Up Policy & Terms of Service */}
-//               <div className="signUpText">By signing up, you agree to Shopee's <a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a></div>
-
-//               {/* Have an account? Log In */}
-//               <div className="haveAnAccount">Have an account?<NavLink to="/login"> Log In</NavLink></div>
-//             </div>
-//           </div>
-//         </div> */}
